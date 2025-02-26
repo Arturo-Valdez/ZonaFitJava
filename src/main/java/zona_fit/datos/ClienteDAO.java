deb7819acfccb07e9fe77c2a1d50152599f4128f
Arturo Valdez <santiguardado27@gmail.com>
2025-02-26 10:56:39 -0600

    Metodo agregar cliente

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -74,6 +74,27 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean agregarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = Conexion.getConexion();
+        String sql = "INSERT INTO cliente(nombre, apellido, membresia)"
+                + " VALUES(?, ?, ?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.execute();
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Error al agregar cliente: " + e.getMessage());
+        }finally {
+            try{
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion");
+            }
+        }
         return false;
     }
 
